8fd2b072f220cbba0f40b6178b27116c224740d1
Kevin Jiang <kevin.jiang@lexisnexis.com>
2017-07-24 16:09:08 +0800

    check

--- alarm-pkg.el
+++ alarm-pkg.el
@@ -1,4 +1,4 @@
 (define-package
-    "alarm"
-    "20170724.1502"
-  "A package to help with alchemy.")
+  "alarm"
+  "20170724.1502"
+  "A package to alarm yourself at some time.")
--- alarm.el
+++ alarm.el
@@ -3,14 +3,17 @@
 ;; Copyright (C) 2017, Emacs guys, all rights reserved.
 
 ;; Author: Kevim Jiang <wenlin1988@126.com>
-;; Version: 0.0.1
+;; Version: 20170724.1502
+;; Keywords: alarm
 ;; Maintainer: Kevin Jaing <wenlin1988@126.com>
 ;; Created: 2017-07-24 12:00:00
-;; Keywords: Alarm
 
 ;;; Commentary:
 
-;; This is Commentary
+;; Set an alarm with `alarm-clock', and enter the time;
+;; cancel the alarm with `alarm-clock-cancel'
+
+;;; Code:
 
 (defvar alarm-clock-timer nil
   "Keep timer so that the user can cancel the alerm.")
@@ -27,7 +30,7 @@
 
 (defun alarm-clock ()
   "Set an alarm.
-The time format is the same accepted by `run-at-time'. 
+The time format is the same accepted by `run-at-time'.
 For example 12:00am."
   (interactive)
   (let ((time (read-string "Time(example, 12:00am): ")))
